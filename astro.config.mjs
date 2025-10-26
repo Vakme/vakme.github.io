472fdcb6239a7273376360872a5c336b139c20c9
Vakme <4bielak.aleksandra@gmail.com>
2025-10-26 17:37:56 +0100

    fix: urls

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,6 @@ import icon from "astro-icon";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://vakme.github.io',
-  base: '/',
   integrations: [preact(), icon(), sitemap({
     filter: (page) =>
       !page.includes("/blog/tags") &&
