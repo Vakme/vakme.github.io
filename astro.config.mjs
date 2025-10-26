2d6b633997746941e69c71745d02c6a16bda0c4c
Vakme <4bielak.aleksandra@gmail.com>
2025-10-26 17:29:59 +0100

    feat: astro action

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,8 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://neonmint.efeele.dev",
+  site: 'https://vakme.github.io',
+  base: '/',
   integrations: [preact(), icon(), sitemap({
     filter: (page) =>
       !page.includes("/blog/tags") &&
